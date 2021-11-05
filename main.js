9b706078583bb35ed1a7602ae1950839144e2bec
dnlMatos <georginedanilo@gmail.com>
2021-11-05 14:45:03 -0300

    conserto interação nav

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-var btnAbrir = document.querySelector('.burger');
-const linha = document.querySelector('.linha')
-const nav = document.getElementById('nav')
+const btnAbrir = document.querySelector(".burger");
+const caixa = document.querySelector(".caixa");
+const linha = document.querySelector(".linha");
+const nav = document.getElementById("nav");
+const li = document.querySelectorAll(".li");
 
-btnAbrir.addEventListener('click', () => {
-   nav.classList.toggle('esconder')
-   linha.classList.toggle('esconde')
+// btnAbrir.addEventListener('click', () => {
+//    nav.classList.toggle('esconder')
+//    linha.classList.toggle('esconde')
+// });
+
+document.addEventListener("click", (e) => {
+  const alvo = e.target;
+  if (alvo == btnAbrir || alvo == linha || alvo == caixa) {
+    nav.classList.toggle("esconder");
+    linha.classList.toggle("esconde");
+   }
+   
+   if (!nav.classList.contains("esconder")) {
+      for (let i = 0; i < li.length; i++) {
+         if(li[i]==alvo){
+            nav.classList.add("esconder");
+            linha.classList.remove("esconde");
+      }
+    }
+  }
 });
