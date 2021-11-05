660f9dd5c8ba903aa6dd989c5e49738b9db250be
dnlMatos <georginedanilo@gmail.com>
2021-11-05 14:47:52 -0300

    conserto interação navegação mobile

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const linha = document.querySelector(".linha");
 const nav = document.getElementById("nav");
 const li = document.querySelectorAll(".li");
 
-// btnAbrir.addEventListener('click', () => {
-//    nav.classList.toggle('esconder')
-//    linha.classList.toggle('esconde')
-// });
-
 document.addEventListener("click", (e) => {
   const alvo = e.target;
   if (alvo == btnAbrir || alvo == linha || alvo == caixa) {
